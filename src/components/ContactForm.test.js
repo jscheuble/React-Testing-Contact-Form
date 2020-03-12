010887a6a0701e8b0bd63829fc2307837284fa87
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-03-12 15:22:04 -0700

    added more tests

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -14,11 +14,37 @@ test('contact form adds customer info to page', () => {
     const messageInput = getByLabelText(/message/i);
 
     fireEvent.change(firstNameInput, {
-        target: { name: 'firstName', value: 'Jana'}
+        target: { name: 'firstName', value: 'Jana' }
+    });
+    fireEvent.change(lastNameInput, {
+        target: { name: 'lastName', value: 'Scheuble' }
+    });
+    fireEvent.change(emailInput, {
+        target: { name: 'email', value: 'jscheubs@ymail.com' }
+    });
+    fireEvent.change(messageInput, {
+        target: { name: 'message', value: 'hello world' }
     });
 
     findByTestId('submit').then(res => {
         fireEvent.click(res);
-    })
-    
+    });
 });
+
+test('contact form has first and last name inputs', () => {
+    const { findAllByText } = render(<ContactForm />);
+    const names = findAllByText(/name/i);
+    expect(names.length === 2);
+});
+
+test('contact form has email input', () => {
+    const { getByLabelText } = render(<ContactForm />);
+    const emailInput = getByLabelText(/email/i);
+    expect(emailInput).toBeInTheDocument();
+});
+
+test('contact form has message input', () => {
+    const { getByLabelText } = render(<ContactForm />);
+    const messageInput = getByLabelText(/message/i);
+    expect(messageInput).toBeInTheDocument();
+})
